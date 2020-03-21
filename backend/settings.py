0876e21b1e778ccd2d9f4aa45e14b64064bb9d97
Soeren Wegener <wegener92@gmail.com>
2020-03-21 16:55:30 +0100

    Re-enable staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# STATIC_ROOT = '/opt/flipdotvsvirus/static'
+STATIC_ROOT = '/opt/flipdotvsvirus/static'
 
 django_heroku.settings(locals())
