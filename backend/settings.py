b621457c4103fc79f6a75c88708f60ef50f15d0a
Soeren Wegener <wegener92@gmail.com>
2020-03-21 15:57:43 +0100

    Disable staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = '/opt/flipdotvsvirus/static'
+# STATIC_ROOT = '/opt/flipdotvsvirus/static'
